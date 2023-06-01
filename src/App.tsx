82f612e943fdcb1b599080c979862f900c56bfae
YK <ykdhan@gmail.com>
2023-06-02 01:13:02 +0900

    Feat: Add sass, prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./assets/scss/common.scss";
 
-async function callApi<T = any>({ url, method }: { url: string; method: string; }) {
+async function callApi<T = any>({
+  url,
+  method,
+}: {
+  url: string;
+  method: string;
+}) {
   const res = await fetch(url, { method });
   const json = await res?.json();
 
   if (!res.ok) {
     throw {
       statusText: res.statusText,
-      json
+      json,
     };
   }
 
@@ -19,89 +25,91 @@ function App() {
   const [fetchResult, setFetchResult] = useState<string[]>([]);
 
   return (
-      <>
-        <button
-            className="button-with-margin"
-            onClick={async () => {
-              try {
-                const json = await callApi<{ messages: string }>({
-                  url: '/test',
-                  method: 'post'
-                });
+    <>
+      <button
+        className="button-with-margin"
+        onClick={async () => {
+          try {
+            const json = await callApi<{ messages: string }>({
+              url: "/test",
+              method: "post",
+            });
 
-                setFetchResult([...fetchResult, JSON.stringify(json.messages)]);
-              } catch (e) {
-                console.log('e', e);
-              }
-            }}
-        >
-          post test
-        </button>
-        <button
-            className="button-with-margin"
-            onClick={async () => {
-              try {
-                const json = await callApi<{ messages: string }>({
-                  url: '/test',
-                  method: 'get'
-                });
+            setFetchResult([...fetchResult, JSON.stringify(json.messages)]);
+          } catch (e) {
+            console.log("e", e);
+          }
+        }}
+      >
+        post test
+      </button>
+      <button
+        className="button-with-margin"
+        onClick={async () => {
+          try {
+            const json = await callApi<{ messages: string }>({
+              url: "/test",
+              method: "get",
+            });
 
-                setFetchResult([...fetchResult, JSON.stringify(json.messages)]);
-              } catch (e) {
-                console.log('e', e);
-              }
-            }}
-        >
-          get test
-        </button>
-        <button
-            className="button-with-margin"
-            onClick={async () => {
-              try {
-                const json = await callApi<{ messages: string }>({
-                  url: '/test',
-                  method: 'put'
-                });
+            setFetchResult([...fetchResult, JSON.stringify(json.messages)]);
+          } catch (e) {
+            console.log("e", e);
+          }
+        }}
+      >
+        get test
+      </button>
+      <button
+        className="button-with-margin"
+        onClick={async () => {
+          try {
+            const json = await callApi<{ messages: string }>({
+              url: "/test",
+              method: "put",
+            });
 
-                setFetchResult([...fetchResult, JSON.stringify(json.messages)]);
-              } catch (e) {
-                console.log('e', e);
-              }
-            }}
-        >
-          put test
-        </button>
-        <button
-            className="button-with-margin"
-            onClick={async () => {
-              try {
-                const json = await callApi<{ messages: string }>({
-                  url: '/test',
-                  method: 'delete'
-                });
+            setFetchResult([...fetchResult, JSON.stringify(json.messages)]);
+          } catch (e) {
+            console.log("e", e);
+          }
+        }}
+      >
+        put test
+      </button>
+      <button
+        className="button-with-margin"
+        onClick={async () => {
+          try {
+            const json = await callApi<{ messages: string }>({
+              url: "/test",
+              method: "delete",
+            });
 
-                setFetchResult([...fetchResult, JSON.stringify(json.messages)]);
-              } catch (e) {
-                console.log('e', e);
-              }
-            }}
-        >
-          delete test
-        </button>
-        <button
-            className="button-with-margin clear"
-            onClick={() => setFetchResult([])}
-        >
-          Clear!
-        </button>
-        <br />
-        <br />
-        {fetchResult?.length > 0 && (
-          <ul className="fetch-result">
-            {[...fetchResult].reverse().map((v, i) => <li key={`${v}-${i}`}>{v}</li>)}
-          </ul>
-        )}
-      </>
+            setFetchResult([...fetchResult, JSON.stringify(json.messages)]);
+          } catch (e) {
+            console.log("e", e);
+          }
+        }}
+      >
+        delete test
+      </button>
+      <button
+        className="button-with-margin clear"
+        onClick={() => setFetchResult([])}
+      >
+        Clear!
+      </button>
+      <br />
+      <br />
+      {fetchResult?.length > 0 && (
+        <ul className="fetch-result">
+          {[...fetchResult].reverse().map((v, i) => (
+            <li key={`${v}-${i}`}>{v}</li>
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
 
